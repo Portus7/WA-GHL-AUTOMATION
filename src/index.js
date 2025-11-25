071f685fc5292dc43f9aa6ca4056482cdaa8e9a0
Portus7 <luisporta7@hotmail.com>
2025-11-25 07:51:30 -0300

    Bayleys-GHL-gemini V2.88

--- src/index.js
+++ src/index.js
@@ -312,7 +312,7 @@ async function startWhatsApp(locationId, slotId) {
 // 📩 UPSERT: Mensajes Entrantes Y Salientes
   sock.ev.on("messages.upsert", async (msg) => {
     try {
-        console.log(msg.messages[0])
+        console.log(msg.messages)
         const m = msg.messages[0];
         if (!m?.message) return;
         
