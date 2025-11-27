89014c21c4afffe27c5c5ba36f8d7876390e5cd3
Portus7 <luisporta7@hotmail.com>
2025-11-27 10:22:43 -0300

    Bayleys-GHL-gemini generacion de botones 2.5

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -276,7 +276,7 @@ async function startWhatsApp(locationId, slotId) {
         console.log(`📩 PROCESANDO: ${clientPhone} (FromMe: ${isFromMe})`);
 
         const route = await getRoutingForPhone(clientPhone, locationId);
-        const messageNumber = route?.slots?.[0]?.messages ?? 1;
+        const messageNumber = route?.messages ?? 1;
         console.log("Nro de mensajes: ",messageNumber, "ROUTE: ", route)
         const existingContactId = (route?.locationId === locationId) ? route.contactId : null;
         const contact = await findOrCreateGHLContact(locationId, clientPhone, waName, existingContactId, isFromMe);
