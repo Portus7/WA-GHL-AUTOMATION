0831c253b9e568e217d13493b630536b1f758581
Portus7 <luisporta7@hotmail.com>
2025-12-11 10:00:16 -0300

    Restructura de codigo y media cleanup 1.1

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -190,7 +190,7 @@ async function handleIncomingMessage(msg, sock, locationId, _poolArg, botMessage
         await logMessageToGHL(locationId, contact.id, messageForGHL, direction, attachments);
 
         if (transcription) {
-            let transcriptionMsg = `🎤 [Transcripción]:\n"${transcription}"\n\nSource: +${myChannelNumber}`;
+            let transcriptionMsg = `🎤 [Transcripción]:\n"${transcription}"\n\nSource: +${sourceLabel}`;
             if (isFromMe) transcriptionMsg += "\n\n[Enviado desde otro dispositivo]";
             await logMessageToGHL(locationId, contact.id, transcriptionMsg, direction, []);
         }
