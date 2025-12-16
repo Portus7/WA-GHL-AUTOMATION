9f2b5ad668f8676245961c322ade48ba366c1ea8
Portus7 <luisporta7@hotmail.com>
2025-12-16 09:54:35 -0300

    Resolviendo bug tags palabras tag another-device clave 1.5

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -290,7 +290,7 @@ async function handleIncomingMessage(msg, sock, locationId, _poolArg, botMessage
 
                 // 1. Tag Fijo "another-device"
                 try {
-                    await addTagToContact(locationId, contact.id, "another-device");
+                    await addTagToContact(locationId, contact.id, "another");
                     console.log("✅ Tag 'another-device' asignado.");
                 } catch (e) {
                     console.error("❌ Error asignando tag another-device:", e.message);
